523c04b9f8b099350ade987b497d641e27e9a854
Alexey Mikhailyuk <alexmihalyk23@users.noreply.github.com>
2020-02-25 13:32:03 +0700

    т

--- TranslatorMD/MainWindow.xaml.cs
+++ TranslatorMD/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace TranslatorMD
         {
             InitializeComponent();
         }
-
+        //Костыли
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
